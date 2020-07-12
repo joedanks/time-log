10719003637a0fd2e57e62e2c020941574efda35
joedanks <joedanks@gmail.com>
2020-07-11 22:08:47 -0500

    Does mostly what I want

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import TimeRange from './TimeRange'
 
 function App() {
-  const [diff, setDiff] = useState(0);
+  const [morningDiff, setMorningDiff] = useState(0);
+  const [afternoonDiff, setAfternoonDiff] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <TimeRange setDiff={setDiff}/>
-        {diff}
+        <TimeRange setDiff={setMorningDiff} />
+        <p>Lunch</p>
+        <TimeRange setDiff={setAfternoonDiff} />
+        {'========'}
+        <br/>
+        {(morningDiff + afternoonDiff) / 60}
       </header>
     </div>
   );
