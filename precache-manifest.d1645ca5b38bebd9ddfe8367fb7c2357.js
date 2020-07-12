1340b007585c8aac4cf2a56b553f0b48ce94483e
joedanks <joedanks@gmail.com>
2020-07-11 22:19:27 -0500

    Updates

---
+++ precache-manifest.d1645ca5b38bebd9ddfe8367fb7c2357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470a5d8977bfd7173b8c9299a12299b8",
+    "url": "/time-log/index.html"
+  },
+  {
+    "revision": "50d38712e47311541c33",
+    "url": "/time-log/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27160f7348b8de5be804",
+    "url": "/time-log/static/js/2.7c2a187e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/time-log/static/js/2.7c2a187e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d38712e47311541c33",
+    "url": "/time-log/static/js/main.84865886.chunk.js"
+  },
+  {
+    "revision": "f4b0f4f825fcb2565bf0",
+    "url": "/time-log/static/js/runtime-main.41a7bc08.js"
+  }
+]);
